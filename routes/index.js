3e90d3d30fa60049b49bce282602dd29f031acd4
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-05-09 17:23:28 +0200

    auth done

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.status(200).json({ title: 'Express' });
+  res.status(200).json({ title: 'Welcome to timeBox server' });
 });
 
 module.exports = router;
