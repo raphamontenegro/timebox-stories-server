802ece257ccfb7fa4131e253810ea39112b14681
Germán Rodríguez <germanrodriguez@MacBook-Pro-de-German.local>
2018-05-15 15:10:07 +0200

    pocket problem solve?

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// @todo dotenv
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -8,7 +9,6 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const bcrypt = require('bcrypt');
-
 const PocketStrategy = require('passport-pocket');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -28,59 +28,6 @@ mongoose.connect('mongodb://localhost/timeBox', {
   reconnectTries: Number.MAX_VALUE
 });
 
-// Middleware
-
-// ----------------- Passport configuration --------------------//
-
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
-const POCKET_CONSUMER_KEY = '77233-bc1d5f96390df6ad14c48477';
-
-const pocketStrategy = new PocketStrategy({
-  consumerKey: POCKET_CONSUMER_KEY,
-  callbackURL: 'http://localhost:3000/auth/pocket/callback'
-}, function (username, accessToken, done) {
-  process.nextTick(function () {
-    return done(null, {
-      username: username,
-      accessToken: accessToken
-    });
-  });
-}
-);
-passport.use(pocketStrategy);
-
-// Passport Set serializers
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
-
-// ----------------- Passport setup --------------------//
-
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:4200']
-  })
-);
-
 // ----------------- Session --------------------//
 
 app.use(
@@ -98,13 +45,76 @@ app.use(
   })
 );
 
+// ----------------- Passport configuration --------------------//
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({ username: username }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { code: 'incorrect' });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return done(null, false, { code: 'incorrect' });
+    }
+    return done(null, user);
+  });
+}));
+
+// Passport Set serializers
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+const POCKET_CONSUMER_KEY = '77233-bc1d5f96390df6ad14c48477';
+
+const options = {
+  consumerKey: POCKET_CONSUMER_KEY,
+  callbackURL: 'http://localhost:3000/auth/pocket/callback' // http changed
+};
+const pocketStrategy = new PocketStrategy(options, (username, accessToken, done) => {
+  User.findOne({ pocketUsername: username }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (user) {
+      return done(null, user);
+    }
+
+    const newUser = new User({
+      pocketUsername: username,
+      pocketToken: accessToken
+    });
+
+    newUser.save(err => {
+      if (err) {
+        return done(err);
+      }
+      done(null, newUser);
+    });
+  });
+});
+passport.use(pocketStrategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:4200']
+  })
+);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', index);
 app.use('/auth', auth);
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,27 @@
 
 const express = require('express');
 const bcrypt = require('bcrypt');
-const router = express.Router();
+const request = require('request');
 const passport = require('passport');
 
+const router = express.Router();
+
 const User = require('../models/user');
+
+const POCKET_CONSUMER_KEY = '77233-bc1d5f96390df6ad14c48477';
+
+const formDataToJson = function (formData) {
+  const json = {};
+
+  formData.split('&').forEach(function (item) {
+    const itemKey = item.split('=')[0];
+    const itemValue = item.split('=')[1];
+
+    json[itemKey] = itemValue;
+  });
+
+  return json;
+};
 
 router.get('/me', (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -53,72 +70,59 @@ router.post('/signup', (req, res, next) => {
     .catch(next);
 });
 
-// router.post('/login', (req, res, next) => {
-//   if (req.session.currentUser) {
-//     return res.status(401).json({ code: 'unauthorized' });
-//   }
-
-//   const username = req.body.username;
-//   const password = req.body.password;
-
-//   if (!username || !password) {
-//     return res.status(422).json({ code: 'validation' });
-//   }
-
-//   User.findOne({ username })
-//     .then((user) => {
-//       if (!user) {
-//         return res.status(404).json({ code: 'not-found' });
-//       }
-//       if (!bcrypt.compareSync(password, user.password)) {
-//         return res.status(404).json({ code: 'not-found' });
-//       } else {
-//         req.session.currentUser = user;
-//         return res.json(user);
-//       }
-//     })
-//     .catch(next);
-// });
-
-router.get('/login/success', (req, res, next) => {
-  if (req.user) {
-    return res.json(req.user);
-  }
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (error, user, info) => {
+    if (error) {
+      return next(error);
+    }
+    if (!user) {
+      return res.status(401).json(info);
+    }
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.json(user);
+    });
+  })(req, res, next);
 });
-
-router.get('/login/failure', (req, res, next) => {
-  return res.status(404).json({ code: 'not-found' });
-});
-
-// router.post('/login', passport.authenticate('local', {
-//   successRedirect: '/auth/login/success',
-//   failureRedirect: '/auth/login/failure',
-//   failureFlash: false,
-//   passReqToCallback: true
-// }));
-
-router.post('/login',
-  passport.authenticate('local'),
-  function (req, res) {
-    return res.json(req.user);
-  }
-);
 
 router.post('/logout', (req, res) => {
   req.logout();
-  return res.status(204).json({code: 'ok'});
+  return res.status(204).json({ code: 'ok' });
 });
 
 // ---------------- Passport authorizations ----------------//
 
-router.get('/pocket', passport.authenticate('pocket'),
-  function (req, res) {
-    return res.json(req.user);
-  });
+router.get('/pocket', (req, res, next) => {
+  const callbackURL = 'localhost:3000/auth/pocket/callback';
 
-router.get('/pocket/callback', passport.authenticate('pocket', { failureRedirect: '/login' }),
-  function (req, res) {
-    return res.json(req.user);
+  request.post({
+    'headers': { 'content-type': 'application/x-www-form-urlencoded' },
+    'url': 'https://getpocket.com/v3/oauth/request',
+    'form': {
+      'consumer_key': POCKET_CONSUMER_KEY,
+      'redirect_uri': callbackURL
+    }
+  }, function (error, response, body) {
+    if (error) { next(error); }
+
+    const data = formDataToJson(body);
+    const url = `https://getpocket.com/auth/authorize?request_token=${data.code}&redirect_uri=${callbackURL}`;
+
+    res.status(200).json({ url });
   });
+});
+
+// @note special route: this is the OAuth callback so it needs to respond with a redirect back to our frontend
+router.get('/pocket/callback', (req, res, next) => {
+  passport.authenticate('pocket', (error, user, info) => {
+    if (error) {
+      console.log(error);
+      return res.redirect('http://localhost:4200/login');
+    }
+    res.redirect('http://localhost:4200/stories');
+  });
+});
 
 module.exports = router;
